da69d8d0c5dc493a148c933f24382f0beaf7608b
Maximistr <maximistr100@gmail.com>
2025-10-04 15:40:45 +0200

    character animations

--- axi.gd
+++ axi.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var right = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +11,44 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if velocity.y > 0 and not $AnimatedSprite2D.animation == "attack_1":
+		$AnimatedSprite2D.play("fall")
+	elif velocity.y < 0 and not $AnimatedSprite2D.animation == "attack_1":
+		$AnimatedSprite2D.play("jump")
+		
+	if velocity == Vector2(0,0) and not $AnimatedSprite2D.animation == "attack_1":
+		$AnimatedSprite2D.play("default")
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction == -1:
+			right = false
+		else :
+			right = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if Input.is_action_just_pressed("attack"):
+		$AnimatedSprite2D.play("attack_1")
+		
+		
+	if velocity.x != 0 and is_on_floor() and not $AnimatedSprite2D.animation == "attack_1":
+		$AnimatedSprite2D.play("run")
+	
+	if right:
+		$AnimatedSprite2D.flip_h = false
+		$attack/CollisionShape2D.position.x = 6
+	else :
+		$AnimatedSprite2D.flip_h = true
+		$attack/CollisionShape2D.position.x = -6
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	$AnimatedSprite2D.play("default")
