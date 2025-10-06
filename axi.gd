32db1bf8316bb1b4492193a75fccbe2a63ea9aed
Maximistr <maximistr100@gmail.com>
2025-10-06 13:36:48 +0200

    dash
    added almoast working dash

--- axi.gd
+++ axi.gd
@@ -3,16 +3,17 @@ extends CharacterBody2D
 var health_image = preload("res://character_assets/hearts.png")
 var half_a_heart = preload("res://character_assets/half heart.png")
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 var right = true
-@export var health  := 4.5
-@export var max_health := 4
+var is_dashing = false
+
 
 func _ready() -> void:
-	pass
+	Global.dash = true
 
 func _process(delta: float) -> void:
 	show_health()
+	$CanvasLayer/Control/Label.text = str(Global.souls)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,12 +24,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if velocity.y > 0 and not $AnimatedSprite2D.animation == "attack_1":
+	if velocity.y > 0 and not $AnimatedSprite2D.animation == "attack_1" and not is_dashing:
 		$AnimatedSprite2D.play("fall")
-	elif velocity.y < 0 and not $AnimatedSprite2D.animation == "attack_1":
+	elif velocity.y < 0 and not $AnimatedSprite2D.animation == "attack_1" and not is_dashing:
 		$AnimatedSprite2D.play("jump")
 		
-	if velocity == Vector2(0,0) and not $AnimatedSprite2D.animation == "attack_1":
+	if velocity == Vector2(0,0) and not $AnimatedSprite2D.animation == "attack_1" and not is_dashing:
 		$AnimatedSprite2D.play("default")
 		
 	# Get the input direction and handle the movement/deceleration.
@@ -43,11 +44,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and not is_dashing:
 		$AnimatedSprite2D.play("attack_1")
 		
 		
-	if velocity.x != 0 and is_on_floor() and not $AnimatedSprite2D.animation == "attack_1":
+	if velocity.x != 0 and is_on_floor() and not $AnimatedSprite2D.animation == "attack_1" and not is_dashing:
 		$AnimatedSprite2D.play("run")
 	
 	if right:
@@ -57,6 +58,24 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = true
 		$attack/CollisionShape2D.position.x = -6
 		
+	if Global.dash:
+		var veloc : float
+		if Input.is_action_just_pressed("dash"):
+			if direction:
+				is_dashing = true
+				veloc = velocity.y
+				$AnimatedSprite2D.play("dash")
+		if Input.is_action_pressed("dash"):
+			if direction:
+				velocity.x = direction * 1000
+				velocity.y = veloc
+				if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
+					Input.action_release("dash")
+			else:
+				Input.action_release("dash")
+		if Input.is_action_just_released("dash"):
+			is_dashing = false
+				
 	move_and_slide()
 
 
@@ -66,10 +85,10 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 func show_health():
 	$CanvasLayer/Control/RichTextLabel.clear()
 
-	for x in range(floor(health)):
+	for x in range(floor(Global.hp_current)):
 		$CanvasLayer/Control/RichTextLabel.add_image(health_image)
 		
-	if health - floor(health) == 0.5:
+	if Global.hp_current - floor(Global.hp_current) == 0.5:
 		$CanvasLayer/Control/RichTextLabel.add_image(half_a_heart)
 
 	
