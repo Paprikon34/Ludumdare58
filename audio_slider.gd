d7f6114de6fd82c37ba640d052eb0d94c2658c50
Patrik Michael Balog <patrikmichaelbalog@gmail.com>
2026-06-15 20:09:33 +0200

    added fullscrean and fixed audio glich

--- audio_slider.gd
+++ audio_slider.gd
@@ -1,13 +1,20 @@
 extends HSlider
 
+@export var audio_bus_name: String = "Master"
 
-@export var audio_bus_name: String
+var audio_bus_id: int
 
-var audio_bus_id
-
-func _ready():
+func _ready() -> void:
+	if audio_bus_name == "":
+		audio_bus_name = "Master"
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
+	if audio_bus_id == -1:
+		audio_bus_id = AudioServer.get_bus_index("Master")
+	
+	if audio_bus_id != -1:
+		value = db_to_linear(AudioServer.get_bus_volume_db(audio_bus_id))
 
 func _on_value_changed(value: float) -> void:
-	var db = linear_to_db(value)
-	AudioServer.set_bus_volume_db(audio_bus_id, db)
+	if audio_bus_id != -1:
+		AudioServer.set_bus_volume_db(audio_bus_id, linear_to_db(value))
+
--- control.gd
+++ control.gd
@@ -1,10 +1,18 @@
 extends Control
 @onready var settings: Panel = $settings
 @onready var main_menu: Control = $main_menu
+@onready var fullscreen_btn: Button = $settings/fullscreen_bg/fullscreen
 
-func _ready():
+func _ready() -> void:
 	main_menu.visible = true
 	settings.visible = false
+	
+	var mode = DisplayServer.window_get_mode()
+	print("[Settings] Initial window mode: ", mode)
+	if mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN or mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		fullscreen_btn.text = "Windowed"
+	else:
+		fullscreen_btn.text = "Fullscreen"
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://tscens/lvl1.tscn")
@@ -23,3 +31,19 @@ func _on_exit_pressed() -> void:
 func _on_back_pressed() -> void:
 	main_menu.visible = true
 	settings.visible = false
+
+
+func _on_fullscreen_pressed() -> void:
+	print("[Settings] Fullscreen button clicked!")
+	var mode = DisplayServer.window_get_mode()
+	print("[Settings] Current mode before toggle: ", mode)
+	if mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN or mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		fullscreen_btn.text = "Fullscreen"
+		print("[Settings] Toggled to Windowed")
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		fullscreen_btn.text = "Windowed"
+		print("[Settings] Toggled to Fullscreen")
+
+
